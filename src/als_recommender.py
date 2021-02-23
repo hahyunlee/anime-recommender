5afb26ce3c9ea37855a19d29c3a8b8dc47a7455d
Josh Lee <jlee@Joshs-MacBook-Pro.local>
2021-02-22 18:16:33 -0800

    complete knn and als class

--- src/als_recommender.py
+++ src/als_recommender.py
@@ -2,6 +2,8 @@ import os
 import argparse
 import time
 import gc
+import math
+import numpy as np
 
 # spark imports
 from pyspark.sql import SparkSession, Row
@@ -10,19 +12,18 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 
 
-
 class AlsRecommender:
     """
-    This a collaborative filtering recommender with Alternating Least Square
-    Matrix Factorization, which is implemented by Spark
+    Class implements alternating least squares from Spark
     """
+
     def __init__(self, spark_session, path_anime, path_ratings):
         self.spark = spark_session
         self.sc = spark_session.sparkContext
-        self.animeDF = self._load_file(path_anime) \
-            .select(['anime_id', 'name'])
-        self.ratingsDF = self._load_file(path_ratings) \
-            .select(['user_id', 'anime_id', 'rating'])
+
+        self.anime_df = self._load_file(path_anime).select(['anime_id', 'name'])
+        self.ratings_df = self._load_file(path_ratings).select(['user_id', 'anime_id', 'rating'])
+
         self.model = ALS(
             userCol='user_id',
             itemCol='anime_id',
@@ -31,12 +32,12 @@ class AlsRecommender:
 
     def _load_file(self, filepath):
         """
-        load csv file into memory as spark DF
+        Load csv file into Spark DF
         """
         return self.spark.read.load(filepath, format='csv',
                                     header=True, inferSchema=True)
 
-    def tune_model(self, maxIter, regParams, ranks, split_ratio=(6, 2, 2)):
+    def tune_model(self, maxIter, regParams, ranks, split_ratio=(0.6, 0.2, 0.2)):
         """
         Hyperparameter tuning for ALS model
         Parameters
@@ -47,34 +48,33 @@ class AlsRecommender:
         split_ratio: tuple, (train, validation, test)
         """
         # split data
-        train, val, test = self.ratingsDF.randomSplit(split_ratio)
+        train, val, test = self.ratings_df.randomSplit(split_ratio)
         # holdout tuning
         self.model = tune_ALS(self.model, train, val,
                               maxIter, regParams, ranks)
         # test model
-        predictions = self.model.transform(test)
+        predictions = self.model.transform(test).na.drop()
         evaluator = RegressionEvaluator(metricName="rmse",
                                         labelCol="rating",
                                         predictionCol="prediction")
         rmse = evaluator.evaluate(predictions)
+
         print('The out-of-sample RMSE of the best tuned model is:', rmse)
-        # clean up
+
+        # garbage clean up
         del train, val, test, predictions, evaluator
         gc.collect()
 
     def set_model_params(self, maxIter, regParam, rank):
         """
-        set model params for pyspark.ml.recommendation.ALS
+        Set model params for pyspark.ml.recommendation.ALS
         Parameters
         ----------
         maxIter: int, max number of learning iterations
         regParams: float, regularization parameter
         ranks: float, number of latent factors
         """
-        self.model = self.model \
-            .setMaxIter(maxIter) \
-            .setRank(rank) \
-            .setRegParam(regParam)
+        self.model = ALS(userCol='user_id', itemCol='anime_id', rank=rank, maxIter=maxIter, regParam=regParam)
 
     def _regex_matching(self, fav_anime):
         """
@@ -82,35 +82,35 @@ class AlsRecommender:
         If no match found, return None
         Parameters
         ----------
-        fav_movie: str, name of user input movie
+        fav_anime: str, name of user input anime
         Return
         ------
-        list of indices of the matching movies
+        list of indices of the matching animes
         """
-        print('You have input movie:', fav_anime)
-        matchesDF = self.animeDF \
+        print('You have input anime:', fav_anime)
+        matches_df = self.anime_df \
             .filter(
-                lower(
-                    col('name')
-                ).like('%{}%'.format(fav_anime.lower()))
-            ) \
+            lower(
+                col('name')
+            ).like('%{}%'.format(fav_anime.lower()))
+        ) \
             .select('anime_id', 'name')
-        if not len(matchesDF.take(1)):
+        if not len(matches_df.take(1)):
             print('Oops! No match is found')
         else:
-            anime_ids = matchesDF.rdd.map(lambda r: r[0]).collect()
-            names = matchesDF.rdd.map(lambda r: r[1]).collect()
+            anime_ids = matches_df.rdd.map(lambda r: r[0]).collect()
+            names = matches_df.rdd.map(lambda r: r[1]).collect()
             print('Found possible matches in our database: '
                   '{0}\n'.format([x for x in names]))
             return anime_ids
 
     def _append_ratings(self, user_id, anime_ids):
         """
-        append a user's movie ratings to ratingsDF
+        append a user's anime ratings to ratings_df
         Parameter
         ---------
         userId: int, userId of a user
-        movieIds: int, movieIds of user's favorite movies
+        anime_ids: int, anime_ids of user's favorite animes
         """
         # create new user rdd
         user_rdd = self.sc.parallelize(
@@ -124,23 +124,23 @@ class AlsRecommender:
             )
         )
         # transform rows to spark DF
-        userDF = self.spark.createDataFrame(user_rows) \
-            .select(self.ratingsDF.columns)
-        # append to ratingsDF
-        self.ratingsDF = self.ratingsDF.union(userDF)
+        user_df = self.spark.createDataFrame(user_rows) \
+            .select(self.ratings_df.columns)
+        # append to ratings_df
+        self.ratings_df = self.ratings_df.union(user_df)
 
     def _create_inference_data(self, user_id, anime_ids):
         """
-        create a user with all movies except ones were rated for inferencing
+        create a user with all animes except ones were rated for inferencing
         """
-        # filter movies
-        other_anime_ids = self.animeDF \
+        # filter animes
+        other_anime_ids = self.anime_df \
             .filter(~col('anime_id').isin(anime_ids)) \
             .select(['anime_id']) \
             .rdd.map(lambda r: r[0]) \
             .collect()
         # create inference rdd
-        inferenceRDD = self.sc.parallelize(
+        inference_raw = self.sc.parallelize(
             [(user_id, anime_id) for anime_id in other_anime_ids]
         ).map(
             lambda x: Row(
@@ -149,58 +149,62 @@ class AlsRecommender:
             )
         )
         # transform to inference DF
-        inferenceDF = self.spark.createDataFrame(inferenceRDD) \
+        inference_df = self.spark.createDataFrame(inference_raw) \
             .select(['user_id', 'anime_id'])
-        return inferenceDF
+        return inference_df
 
-    def _inference(self, model, fav_anime, n_recommendations):
+    def _make_inference(self, model, fav_anime, n_recommendations):
         """
-        return top n movie recommendations based on user's input movie
+        return top n anime recommendations based on user's input anime
         Parameters
         ----------
         model: spark ALS model
-        fav_movie: str, name of user input movie
+        fav_anime: str, name of user input anime
         n_recommendations: int, top n recommendations
         Return
         ------
-        list of top n similar movie recommendations
+        list of top n similar anime recommendations
         """
         # create a userId
-        user_id = self.ratingsDF.agg({"userId": "max"}).collect()[0][0] + 1
-        # get movieIds of favorite movies
+        user_id = self.ratings_df.agg({"user_id": "max"}).collect()[0][0] + 1
+        # get anime_ids of favorite animes
         anime_ids = self._regex_matching(fav_anime)
         # append new user with his/her ratings into data
         self._append_ratings(user_id, anime_ids)
         # matrix factorization
-        model = model.fit(self.ratingsDF)
+        model = model.fit(self.ratings_df)
         # get data for inferencing
-        inferenceDF = self._create_inference_data(user_id, anime_ids)
+        inference_df = self._create_inference_data(user_id, anime_ids)
         # make inference
-        return model.transform(inferenceDF) \
-            .select(['anime_id', 'prediction']) \
+
+        results = model.transform(inference_df) \
+            .select(['anime_id', 'prediction']).na.drop()
+
+        return results \
             .orderBy('prediction', ascending=False) \
             .rdd.map(lambda r: (r[0], r[1])) \
             .take(n_recommendations)
 
     def make_recommendations(self, fav_anime, n_recommendations):
         """
-        make top n movie recommendations
+        make top n anime recommendations
         Parameters
         ----------
-        fav_movie: str, name of user input movie
+        fav_anime: str, name of user input anime
         n_recommendations: int, top n recommendations
         """
         # make inference and get raw recommendations
         print('Recommendation system start to make inference ...')
         t0 = time.time()
         raw_recommends = \
-            self._inference(self.model, fav_anime, n_recommendations)
+            self._make_inference(self.model, fav_anime, n_recommendations)
         anime_ids = [r[0] for r in raw_recommends]
         scores = [r[1] for r in raw_recommends]
+
         print('It took my system {:.2f}s to make inference \n\
               '.format(time.time() - t0))
-        # get movie titles
-        anime_titles = self.animeDF \
+        # get anime titles
+        anime_titles = self.anime_df \
             .filter(col('anime_id').isin(anime_ids)) \
             .select('name') \
             .rdd.map(lambda r: r[0]) \
@@ -209,37 +213,7 @@ class AlsRecommender:
         print('Recommendations for {}:'.format(fav_anime))
         for i in range(len(anime_titles)):
             print('{0}: {1}, with rating '
-                  'of {2}'.format(i+1, anime_titles[i], scores[i]))
-
-
-class Dataset:
-    """
-    data object make loading raw files easier
-    """
-    def __init__(self, spark_session, filepath):
-        """
-        spark dataset constructor
-        """
-        self.spark = spark_session
-        self.sc = spark_session.sparkContext
-        self.filepath = filepath
-        # build spark data object
-        self.RDD = self.load_file_as_RDD(self.filepath)
-        self.DF = self.load_file_as_DF(self.filepath)
-
-    def load_file_as_RDD(self, filepath):
-        ratings_RDD = self.sc.textFile(filepath)
-        header = ratings_RDD.take(1)[0]
-        return ratings_RDD \
-            .filter(lambda line: line != header) \
-            .map(lambda line: line.split(",")) \
-            .map(lambda tokens: (int(tokens[0]), int(tokens[1]), float(tokens[2]))) # noqa
-
-    def load_file_as_DF(self, filepath):
-        ratings_RDD = self.load_file_as_rdd(filepath)
-        ratingsRDD = ratings_RDD.map(lambda tokens: Row(
-            user_id=int(tokens[0]), anime_id=int(tokens[1]), rating=float(tokens[2]))) # noqa
-        return self.spark.createDataFrame(ratingsRDD)
+                  'of {2}'.format(i + 1, anime_titles[i], scores[i]))
 
 
 def tune_ALS(model, train_data, validation_data, maxIter, regParams, ranks):
@@ -249,8 +223,8 @@ def tune_ALS(model, train_data, validation_data, maxIter, regParams, ranks):
     Parameters
     ----------
     model: spark ML model, ALS
-    train_data: spark DF with columns ['userId', 'movieId', 'rating']
-    validation_data: spark DF with columns ['userId', 'movieId', 'rating']
+    train_data: spark DF with columns ['userId', 'anime_id', 'rating']
+    validation_data: spark DF with columns ['userId', 'anime_id', 'rating']
     maxIter: int, max number of learning iterations
     regParams: list of float, one dimension of hyper-param tuning grid
     ranks: list of float, one dimension of hyper-param tuning grid
@@ -259,18 +233,21 @@ def tune_ALS(model, train_data, validation_data, maxIter, regParams, ranks):
     The best fitted ALS model with lowest RMSE score on validation data
     """
     # initial
-    min_error = float('inf')
+    min_error = math.inf
     best_rank = -1
     best_regularization = 0
     best_model = None
     for rank in ranks:
         for reg in regParams:
             # get ALS model
-            als = model.setMaxIter(maxIter).setRank(rank).setRegParam(reg)
+            # als = model.setMaxIter(maxIter).setRank(rank).setRegParam(reg)
+            als = ALS(userCol='user_id', itemCol='anime_id', rank=rank, maxIter=maxIter, regParam=reg)
             # train ALS model
             model = als.fit(train_data)
             # evaluate the model by computing the RMSE on the validation data
             predictions = model.transform(validation_data)
+            # drop na predictions
+            predictions = predictions.na.drop()
             evaluator = RegressionEvaluator(metricName="rmse",
                                             labelCol="rating",
                                             predictionCol="prediction")
@@ -285,3 +262,47 @@ def tune_ALS(model, train_data, validation_data, maxIter, regParams, ranks):
     print('\nThe best model has {} latent factors and '
           'regularization = {}'.format(best_rank, best_regularization))
     return best_model
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="Anime Recommender",
+        description="Run ALS Anime Recommender")
+    parser.add_argument('--proj_path', nargs='?', default='../data',
+                        help='input data path')
+    parser.add_argument('--anime_filename', nargs='?', default='anime.csv',
+                        help='provide anime filename')
+    parser.add_argument('--ratings_filename', nargs='?', default='rating.csv',
+                        help='provide ratings filename')
+    parser.add_argument('--anime_title', nargs='?', default='',
+                        help='provide your favorite anime title')
+    parser.add_argument('--top_n', type=int, default=10,
+                        help='top n anime recommendations')
+    return parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    # get args
+    args = parse_args()
+    data_path = args.proj_path
+    anime_filename = args.anime_filename
+    ratings_filename = args.ratings_filename
+    anime_title = args.anime_title
+
+    top_n = args.top_n
+    # initial spark
+    spark = SparkSession \
+        .builder \
+        .appName("anime recommender") \
+        .getOrCreate()
+    # initial recommender system
+    recommender = AlsRecommender(
+        spark,
+        os.path.join(data_path, anime_filename),
+        os.path.join(data_path, ratings_filename))
+    # set params
+    recommender.set_model_params(10, 0.05, 20)
+    # make recommendations
+    recommender.make_recommendations(anime_title, top_n)
+    # stop
+    spark.stop()
--- src/knn_recommender.py
+++ src/knn_recommender.py
@@ -1,20 +1,122 @@
 import pandas as pd
-from scipy.sparse import csr_matrix
+import os
 from sklearn.neighbors import NearestNeighbors
+from scipy.sparse import csr_matrix
+from fuzzywuzzy import fuzz
+
+class KnnRecommender:
+    """
+        Class implements k Nearest Neighbors clustering
+    """
+    def __init__(self, anime_path, ratings_path):
+        self.anime_df = self._load_file(anime_path)
+        self.ratings_df = self._load_file(ratings_path)
+        # self.model = NearestNeighbors()
+        self.model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+
+    def _load_file(self, file_path):
+        """
+        Load csv file into pandas DF
+        """
+        return pd.read_csv(file_path)
+
+    def _prepare_data(self):
+        ratings_df = self.ratings_df.groupby(['user_id', 'anime_id']).mean().reset_index()
+        user_anime_mat = ratings_df.pivot(
+            index='anime_id',
+            columns='user_id',
+            values='rating'
+        )
+        user_anime_mat.fillna(0,inplace=True)
+        return csr_matrix(user_anime_mat.values)
+
+    def _create_hashmap(self):
+        anime_titles=self.anime_df[['anime_id','name']]
+        anime_titles=anime_titles.set_index('anime_id')
+
+        return {
+            anime: i for i, anime in
+            enumerate(list(anime_titles.name))
+        }
+
+    def _fuzzy_match(self,hash_dict,fav_anime):
+        match_tuple = []
+
+        for title, idx in hash_dict.items():
+            ratio = fuzz.ratio(title.lower(), fav_anime.lower())
+            if ratio >= 60:
+                match_tuple.append((title, idx, ratio))
+
+        match_tuple = sorted(match_tuple, key=lambda x: x[2])[::-1]
+
+        if not match_tuple:
+            print('No match found.')
+        else:
+            print('Found possible matches in database: {0}'.format([x[0] for x in match_tuple]))
+            return match_tuple[0][1]
+
+
+    def make_recommendations(self,fav_anime,n_recommendations):
+        """
+        return top n anime recommendations based on user's input anime
+
+        Parameters
+        ----------
+        fav_anime: str, name of user input anime
+        n_recommendations: int, top n recommendations
+
+        Return
+        ------
+        list of top n similar anime recommendations
+        """
+
+        hash_dict = self._create_hashmap()
+        idx = self._fuzzy_match(hash_dict,fav_anime)
+        sparse_mat = self._prepare_data()
+
+        model = self.model.fit(sparse_mat)
+        distance,indices = model.kneighbors(sparse_mat[idx],n_neighbors=n_recommendations+1)
+
+        raw_recommends = sorted(
+            list(
+                zip(indices.squeeze().tolist(), distance.squeeze().tolist())
+            ),
+            key=lambda x: x[1])[:0:-1]
+
+        reverse_hashmap = {v: k for k, v in hash_dict.items()}
+
+        for i, (idx, dist) in enumerate(raw_recommends):
+            print('{0}: {1}, with distance '
+                  'of {2}'.format(i + 1, reverse_hashmap[idx], dist))
+
+
+if __name__ == '__main__':
+    # get args
+    # args = parse_args()
+    # data_path = args.proj_path
+    # anime_filename = args.anime_filename
+    # ratings_filename = args.ratings_filename
+    # anime_title = args.anime_title
+    # top_n = args.top_n
+    data_path = '../data'
+    anime_filename = 'anime.csv'
+    ratings_filename = 'rating.csv'
+
+    # initial recommender system
+    recommender = KnnRecommender(
+        os.path.join(data_path, anime_filename),
+        os.path.join(data_path, ratings_filename))
+
+    anime = 'Hunter X'
+    top_n = 10
+    recommender.make_recommendations(anime,top_n)
 
 
 
-def prepare_anime_data(df):
-    df_final = df.copy()
-
-    # drop anime w/ nan for genre and type
-    df_final = df_final.dropna(subset=['genre','type'])
-
-    # drop ratings? episode count?
-    # feature engineering: dummify genre and type
 
 
 
-    return df_final
 
 
+    # make recommendations
+    # recommender.make_recommendations(anime_title, top_n)
